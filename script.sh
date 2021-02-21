052a6e128b4fdf7cde994f2e70e4e0865220bd6c
Amin Laanaya <43217357+Bobcycle@users.noreply.github.com>
2021-02-21 21:19:04 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts
 rm -rf main.css
-echo "@import url('https://oreokitten.github.io/latest/dark-n-purple/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
+echo "@import url('https://raw.githubusercontent.com/Bobcycle/Ptero/master/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
 rm -rf index.tsx
 wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/index.tsx
 cd ../..
@@ -12,7 +12,7 @@ echo "User panel theme has been added."
 echo "Installing admin theme..."
 cd /var/www/pterodactyl/resources/views/layouts/
 rm -rf admin.blade.php
-wget https://raw.githubusercontent.com/DeveloperNeon/ptero-1.0-theme-install/main/resources/dark-n-purple/admin.blade.php
+wget https://raw.githubusercontent.com/Bobcycle/Ptero/master/admin.blade.php
 npm install yarn -g
 yarn install
 yarn add @emotion/react
